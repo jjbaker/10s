4e0c685a552411c5571406e68a721dbbad333900
jjbaker <48622798+jjbaker@users.noreply.github.com>
2022-09-07 00:48:40 -0400

    added roll holding functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React from "react";
 import Dice from "./components/Dice.js"
+import {nanoid} from "nanoid"
 import "./style.css";
 
 export default function App() {
   const [diceList, setDiceList] = React.useState(createDice())
+ 
 
-  function createDice(len=10){
-    return Array.from({length: len}, () => Math.ceil(Math.random()*6));
+  function createDice(){
+    const newDice=[]
+    for(let i = 0; i <10; i++){
+      newDice.push({
+        value: Math.ceil(Math.random()*6),
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+    return newDice
   }
 
   function rollDice() {
-    const newRoll = createDice();
-    setDiceList(newRoll)
+    setDiceList(oldRoll => {
+      const newRoll = [];
+      for(let i = 0; i < diceList.length; i++){
+        if(oldRoll[i].isHeld){
+          newRoll.push(oldRoll[i])
+        }
+        else{
+          newRoll.push({
+            ...oldRoll[i],
+            value: Math.ceil(Math.random()*6),
+          })
+        }
+      }
+      return newRoll
+    })
+  }
+  function holdDice(id){
+    setDiceList(oldRoll =>{
+      const newRoll = [];
+      for(let i = 0; i < diceList.length; i++){
+        if(oldRoll[i].id != id){
+          newRoll.push(oldRoll[i])
+        }
+        else{
+          newRoll.push({
+            ...oldRoll[i],
+            isHeld: !oldRoll[i].isHeld,
+          })
+        }
+      }
+      return newRoll
+    })
   }
 
-  const diceElements = diceList.map(d => <Dice value={d} />)
+  const diceElements = diceList.map(d => <Dice key={d.id} value={d.value} id={d.id} onClick={holdDice} isHeld={d.isHeld} />)
 
   const dices = createDice
   return (
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 
 export default function Dice(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
   return (
-    <div className="dice--num">
+    <div 
+      className="dice--num"
+      onClick={(event) => props.onClick(props.id)}
+      style={styles}
+    >
       <h2>{props.value}</h2>
     </div>
   )
